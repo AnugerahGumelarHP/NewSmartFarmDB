f08585970e8d159f203038346222dd46e7e2727b
Agum Whistler <agumwhistler@gmail.com>
2019-09-24 17:07:12 +0700

    Home clear

--- app/src/main/java/com/battistradadeveloper/smartfarm/Pembeli/Home.java
+++ app/src/main/java/com/battistradadeveloper/smartfarm/Pembeli/Home.java
@@ -3,11 +3,11 @@ package com.battistradadeveloper.smartfarm.Pembeli;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.battistradadeveloper.smartfarm.R;
 
@@ -22,6 +22,33 @@ public class Home extends Fragment {
 	}
 	@Override
 	public void onViewCreated(View view, Bundle savedInstanceState) {
+		cv_putih = view.findViewById(R.id.cv_pembeli_putih);
+		cv_putih.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent pembeli_putih = new Intent(getActivity(), Discover_Rice_Putih.class);
+				startActivity(pembeli_putih);
+			}
+		});
+
+		cv_merah = view.findViewById(R.id.cv_pembeli_merah);
+		cv_merah.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent pembeli_merah = new Intent(getActivity(), Discover_Rice_Merah.class);
+				startActivity(pembeli_merah);
+			}
+		});
+
+		cv_hitam = view.findViewById(R.id.cv_pembeli_hitam);
+		cv_hitam.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent pembeli_hitam = new Intent(getActivity(), Discover_Rice_Hitam.class);
+				startActivity(pembeli_hitam);
+			}
+		});
+
 		cv_semua = view.findViewById(R.id.cv_pembeli_semua);
 		cv_semua.setOnClickListener(new View.OnClickListener() {
 			@Override
